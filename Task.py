d037d00e53a9275a0ad5819fd22081a0a61c7246
Danial-Zaheer <f233082@cfd.nu.edu.pk>
2024-07-30 00:52:51 +0500

    Written half of the implementation of Task Class

--- Task.py
+++ Task.py
@@ -1,7 +1,19 @@
 
 class Task:
-    def __init__(self,name,description):
-        self.name = name
+    """A class to model tasks having a title and description"""
+    
+    def __init__(self,title,description):
+        """Initialize the member variable"""
+        self.title = title
         self.description = description
+
+    def __str__(self):
+        return f"Title: {self.title}\nDescription: {self.description}"
+    
+
+a = Task("D","NNNNN")
+
+print("1:",a)
+
 
     
