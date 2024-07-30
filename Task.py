f662fd87e343081de94101b7bebd8905c56f6002
Danial-Zaheer <f233082@cfd.nu.edu.pk>
2024-07-30 15:17:47 +0500

    Added __repr__() method for task class and a class method to convert a string to a Task object

--- Task.py
+++ Task.py
@@ -1,13 +1,18 @@
 
 class Task:
-    """A class to model tasks having a title and description"""
+    """A class to model task having a title and description"""
     
     def __init__(self,title,description):
         """Initialize the member variable"""
         self.title = title.upper()
         self.description = description
 
+    def __repr__(self):
+        """Return the dtring format of task to save in json file"""
+        return f"{self.title}-{self.description}"
+
     def __str__(self):
+        """To display yo end user"""
         return f"{self.title}:\n\t{self.description}\n"
     
     #Getter/Seters
@@ -27,3 +32,7 @@ class Task:
     def description(self, description):
         self._description = description
 
+    @classmethod
+    def from_str(cls,str):
+        """Covert a string back to Task Object"""
+        tile,description = str.split('-')
--- Task_Manager.py
+++ Task_Manager.py
@@ -1,1 +1,11 @@
 from Task import Task
+
+class TaskManager:
+    """
+    This class with model a Task_Manger that will let user
+    add/delete/update/view tasks
+    """
+
+    def __init__():
+        """Initializing the main list"""
+        tasks = []
